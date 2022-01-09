66d2176f7efa89373296620bb66ab04e3d2f79b8
Augusto <augus_borromeo@hotmail.com>
2022-01-09 19:59:30 -0300

    making mobile menu

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
-const video = document.getElementById('website-profile--video')
+const menuIcon = document.querySelector('#menuIcon')
+menu = document.querySelector('#menu-content')
 
+menuIcon.addEventListener('click', (e) => {
+
+    menu.classList.toggle('activate');
+    document.body.classList.toggle('opacity')
+
+    const route = e.target.getAttribute('src');
+
+    if(route == "../imgs/main/mobile/hamburger-icon.png") {
+        e.target.setAttribute('src', "../imgs/main/mobile/hamburger-icon-2.png")
+    } else {
+        e.target.setAttribute('src', "../imgs/main/mobile/hamburger-icon.png")
+    }
+})
